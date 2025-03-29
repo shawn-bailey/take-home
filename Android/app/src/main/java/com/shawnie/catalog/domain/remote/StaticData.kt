cdba431dad260476ca2984aa128db9ba22039d10
shawn-bailey <shawn_bailey2@outlook.com>
2025-03-29 19:35:49 -0400

    backup data

---
+++ Android/app/src/main/java/com/shawnie/catalog/domain/remote/StaticData.kt
@@ -0,0 +1,95 @@
+package com.shawnie.catalog.domain.remote
+
+const val STATIC_DATA = "[\n" +
+        "  {\n" +
+        "    \"name\": \"Avocado Toast\",\n" +
+        "    \"price\": \"5.99\",\n" +
+        "    \"id\": 1,\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2020-11-28T15:14:22Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Bacon Toast\",\n" +
+        "    \"id\": 2,\n" +
+        "    \"price\": \"1.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2021-01-30T02:24:04Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Crunchy Toast\",\n" +
+        "    \"id\": 3,\n" +
+        "    \"price\": \"0.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2021-03-17T03:45:47Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Egg in the basket\",\n" +
+        "    \"id\": 4,\n" +
+        "    \"price\": \"2.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2020-07-12T02:43:38Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"French Toast\",\n" +
+        "    \"id\": 5,\n" +
+        "    \"price\": \"3.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2021-04-11T14:45:28Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Milk Toast\",\n" +
+        "    \"id\": 6,\n" +
+        "    \"price\": \"1.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2020-08-05T13:39:11Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Ogura Toast\",\n" +
+        "    \"id\": 7,\n" +
+        "    \"price\": \"4.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2020-09-11T23:59:58Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Prawn Sesame Toast\",\n" +
+        "    \"id\": 8,\n" +
+        "    \"price\": \"5.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2021-06-20T23:13:28Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Toast Hawaii\",\n" +
+        "    \"id\": 9,\n" +
+        "    \"price\": \"3.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2020-12-28T21:37:20Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Hummus Toast\",\n" +
+        "    \"id\": 10,\n" +
+        "    \"price\": \"1.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2022-03-23T12:38:26Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Whipped Ricotta Toast\",\n" +
+        "    \"id\": 11,\n" +
+        "    \"price\": \"2.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2022-07-30T09:13:34Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Sweet Potato Toast\",\n" +
+        "    \"id\": 12,\n" +
+        "    \"price\": \"3.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2022-01-15T07:03:09Z\"\n" +
+        "  },\n" +
+        "  {\n" +
+        "    \"name\": \"Tuna Toast\",\n" +
+        "    \"id\": 13,\n" +
+        "    \"price\": \"2.99\",\n" +
+        "    \"currency\": \"EUR\",\n" +
+        "    \"last_sold\": \"2022-03-16T10:55:17Z\"\n" +
+        "  }\n" +
+        "]"
