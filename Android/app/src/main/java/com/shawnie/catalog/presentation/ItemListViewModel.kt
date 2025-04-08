1d11d5bc468a725510222656310d0923efa7f12c
shawn-bailey <shawn_bailey2@outlook.com>
2025-04-08 15:13:35 -0400

    Update view model

--- Android/app/src/main/java/com/shawnie/catalog/presentation/ItemListViewModel.kt
+++ Android/app/src/main/java/com/shawnie/catalog/presentation/ItemListViewModel.kt
@@ -2,31 +2,55 @@ package com.shawnie.catalog.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.shawnie.catalog.common.Resource
 import com.shawnie.catalog.domain.data.ItemUseCase
 import com.shawnie.catalog.domain.data.ItemsRepository
 import com.shawnie.catalog.domain.data.ItemsRepositoryImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * View model that transfers toast items state to the compose view
  */
-class ItemListViewModel(
-    val itemUseCase: ItemUseCase = ItemUseCase()
+class ItemListViewModel @Inject constructor (
+    val itemUseCase: ItemUseCase
 ): ViewModel() {
 
     val stateFlow: MutableStateFlow<ItemListState> = MutableStateFlow(ItemListState())
 
+    
+
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            val items = itemsRepository.getItems()
-            stateFlow.update { currentState ->
-                currentState.copy(
-                    itemList = items,
-                    loading = false
-                )
+            itemUseCase().onEach { result ->
+                when(result) {
+                    is Resource.Success -> {
+                        stateFlow.update { currentState ->
+                            currentState.copy(
+                                itemList = result.data ?: emptyList(),
+                                loading = false
+                            )
+                        }
+                    }
+                    is Resource.Error<*> ->  {
+                        stateFlow.update { currentState ->
+                            currentState.copy(
+                                loading = false
+                            )
+                        }
+                    }
+                    is Resource.Loading<*> -> {
+                        stateFlow.update { currentState ->
+                            currentState.copy(
+                                loading = true
+                            )
+                        }
+                    }
+                }
             }
         }
     }
